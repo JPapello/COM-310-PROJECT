71d45fcf9b08cebf93a11e4035a4abc4a5916b95
Jude Andre <judedandre@gmail.com>
2018-11-08 18:50:53 -0500

    Update CPUApplication.java

--- CPUApplication.java
+++ CPUApplication.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cpu.scheduler;
 
 import java.awt.Dimension;
 import javax.swing.JFrame;
@@ -12,12 +6,12 @@ import javax.swing.JFrame;
  *
  * @author Jandre
  */
-public class CPUScheduler {
+public class CPUApplication {
     
     public static void CreateAndShowGUI() //creates application
     {
         JFrame frame = new JFrame("CPU Simulator");
-        frame.setPreferredSize(new Dimension (800, 620)); //Sets dimension to 800 x 800 px
+        frame.setPreferredSize(new Dimension (700, 620)); //Sets dimension to 800 x 800 px
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Allows application to close
         frame.setEnabled(true); //Allows application to accept certain keynotes
