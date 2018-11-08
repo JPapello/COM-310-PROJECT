72405a0c3fcdc271e30571a61b124107fa150ae5
Jude Andre <judedandre@gmail.com>
2018-11-08 15:34:26 -0500

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,4 +1,4 @@
-package cpu.simulator;
+
 
 import javax.swing.JOptionPane;
 
@@ -10,28 +10,38 @@ public class Process {
     private int idNum;
     private int burstTime;
     private int arrivalTime;
+    private int priority;
+    private boolean priorityIncluded;
     
     public Process()
     {
         this.idNum = 0;
         this.burstTime = 0;
         this.arrivalTime = 0;
+        this.priority = 0;
+        this.priorityIncluded = false;
     }
     
-    public Process(int idNum, int burstTime, int arrivalTime)
+    public Process(int idNum)
+    {
+        this.idNum = idNum;
+        this.burstTime = 0;
+        this.arrivalTime = 0;
+        this.priority = 0;
+        this.priorityIncluded = false;
+    }
+    
+    public Process(int idNum, int burstTime, int arrivalTime, int priority, boolean priorityIncluded)
     {
         this.idNum = idNum;
         this.burstTime = burstTime;
         this.arrivalTime = arrivalTime;
+        this.priority = priority;
+        this.priorityIncluded = priorityIncluded;
     }
     
     public void input()
     {
-        int newID = Integer.parseInt(JOptionPane.showInputDialog("Please insert the ID number of the process. \n(If there already exists an ID number and you don't want to change it, input '-1')"));
-        if(newID != -1)
-        {
-            setID(newID);
-        }
         int newBurstTime = Integer.parseInt(JOptionPane.showInputDialog("Please insert the burst time of the process. \n(If there already exists a burst time and you don't want to change it, input '-1')"));
         if(newBurstTime != -1)
         {
@@ -42,12 +52,32 @@ public class Process {
         {
             setArrivalTime(newArrivalTime);
         }
+        Object[] trueOrFalse = {"Yes", "No"};
+        String newPriorityIncluded = (String)(JOptionPane.showInputDialog(null, "Will priority be included in this process?", "Add Priority", JOptionPane.QUESTION_MESSAGE, null, trueOrFalse, trueOrFalse[0]));
+        if(newPriorityIncluded.equals("Yes"))
+        {
+            setPriorityInclude(true);
+        }
+        else
+        {
+            setPriorityInclude(false);
+        }
+        
+        if(priorityIncluded)
+        {
+            int newPriority = Integer.parseInt(JOptionPane.showInputDialog("Please insert the priority of the process. \n(If there already exists an ID number and you don't want to change it, input '-1')"));
+            if(newPriority != -1)
+            {
+                setPriority(newPriority);
+            }
+        }
+        
         System.out.println(getID() + " " + getBurstTime() + " " + getArrivalTime());
     }
     
     public Process deepCopy()
     {
-        return new Process(idNum, burstTime, arrivalTime);
+        return new Process(idNum, burstTime, arrivalTime, priority, priorityIncluded);
     }
     
     public void setID(int idNum)
@@ -65,6 +95,16 @@ public class Process {
         this.arrivalTime = arrivalTime;
     }
     
+    public void setPriority(int priority)
+    {
+        this.priority = priority;
+    }
+    
+    public void setPriorityInclude(boolean priorityIncluded)
+    {
+        this.priorityIncluded = priorityIncluded;
+    }
+    
     public int getID()
     {
         return this.idNum;
@@ -78,5 +118,15 @@ public class Process {
     public int getArrivalTime()
     {
         return this.arrivalTime;
+    }
+    
+    public int getPriority()
+    {
+        return this.priority;
+    }
+    
+    public boolean getPriorityIncluded()
+    {
+        return this.priorityIncluded;
     }
 }
